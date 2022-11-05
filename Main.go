69d8c1482ca3ef7b43b2652a00e8b64f61d07d4f
gonzalogaticao <gonzalogaticaosorio@gmail.com>
2022-11-04 21:38:46 -0300

    InsertUser

--- Main.go
+++ Main.go
@@ -15,99 +15,10 @@ func main() {
 
 	insertQuestion(connectionEstablished)
 	insertPatient(connectionEstablished)
-
-	/*var question string
-	fmt.Print("Formula tu pregunta: ")
-	fmt.Scanln(&question)
-
-	//InsertUser, err := connectionEstablished.Prepare("INSERT INTO usuarios (nombre, edad) VALUES ('Charles', '33') ")
-	InsertUser, err := connectionEstablished.Prepare("INSERT INTO questions (ID, QUESTION, DESCRIPTION_Q, DELETE_AT, TEST_ID) VALUES (NULL, ?, 'Conocer patron de sueño', NULL, NULL); ")
-
-	if err != nil {
-		panic(err.Error())
-	}
-	InsertUser.Exec(question)*/
-
-	/*
-		fmt.Println("TITULO")
-		fmt.Println("Inicio del cuestionario")
-		//1.-Imprimir Instrucicciones.
-		/*--- Extraer informacion en BD.---
-
-		//2.-Recibir al participante.
-		var day int
-		fmt.Print("Ingresar dia: ")
-		fmt.Scanln(&day)
-
-		var month int
-		fmt.Print("Ingresar mes: ")
-		fmt.Scanln(&month)
-
-		var year int
-		fmt.Print("Ingresar anio: ")
-		fmt.Scanln(&year)
-
-		var rut string
-		fmt.Print("Ingresar rut: ")
-		fmt.Scanln(&rut)
-
-		var name string
-		fmt.Print("Ingresar nombre: ")
-		fmt.Scanln(&name)
-
-		var lastName string
-		fmt.Print("Ingresar apellido: ")
-		fmt.Scanln(&lastName)
-
-		var age int
-		fmt.Print("Ingresar edad: ")
-		fmt.Scanln(&age)
-		/*--- Guardar informacion en BD.--- */
-
-	//3.-Realizar preguntas.
-	/*--- Extraer informacion de BD.---
-
-	//4.-Obtener respuestas.
-	var answers [10]int
-	var num int
-	var i int
-
-	for i = 0; i < 10; i++ { //relleno
-		fmt.Printf("Respuesta de pregunta (%v): ", i+1)
-		fmt.Scanln(&num)
-		if num >= 1 && num <= 3 {
-			answers[i] = num
-		} else {
-			i--
-		}
-	}
-	/*--- Guardar informacion en BD.---
-
-	//5.-Calcular resultados.
-	var result int
-	for i = 0; i < 10; i++ {
-		result = result + answers[i]
-	}
-	/*--- Guardar informacion en BD.---
-
-	//6.-Mostrar resultados.
-	fmt.Printf("El resultado es: %v\n", result) //Respuesta arbitraria.
-	if result < 10 {
-		fmt.Printf("Participante %v %v se encuentra en buen estado.\n", name, lastName)
-	}
-	if result >= 10 && result <= 20 {
-		fmt.Printf("Participante %v %v se encuentra en condicion estable.\n", name, lastName)
-	}
-	if result > 20 && result <= 25 {
-		fmt.Printf("Participante %v %v requiere atencion.\n", name, lastName)
-	}
-	if result > 25 {
-		fmt.Printf("Participante %v %v se le diagnostica depresion.\n", name, lastName)
-	}
+	insertUser(connectionEstablished)
 
 	//7.-Cerrar y finalizar.
 	fmt.Println("FIN.")
-	*/
 }
 
 func conectionBD() (conection *sql.DB) {
@@ -140,7 +51,7 @@ func insertQuestion(connectionEstablished *sql.DB) {
 	}
 	InsertQuestion.Exec(question, description)
 
-	fmt.Println("Pregunta Ingresada con exito.")
+	fmt.Println("Pregunta ingresada con exito.")
 }
 
 func insertPatient(connectionEstablished *sql.DB) {
@@ -177,12 +88,51 @@ func insertPatient(connectionEstablished *sql.DB) {
 	o := bufio.NewReader(os.Stdin)
 	observation, _ := o.ReadString('\n')
 
-	//INSERT INTO `patient` (`RUN`, `DV`, `NAMES`, `FATHERNAME`, `MOTHERNAME`, `PHONE`, `MAIL`, `OBSERVATION`, `ID`, `ACTIVE`) VALUES ('28246613', 'k', 'Charles_Juaquin', 'Garcia', 'Jimenez', '999999', 'juja@gmail.com', 'Se demoro en contestar el cuestionario', NULL, '1');
-	insertPatient, err := connectionEstablished.Prepare("INSERT INTO patient (ID, RUN, DV, NAMES, FATHERNAME, MOTHERNAME, PHONE, MAIL, OBSERVATION, ACTIVE) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, '1')")
+	insertPatient, err := connectionEstablished.Prepare("INSERT INTO patients (ID, RUN, DV, NAMES, FATHERNAME, MOTHERNAME, PHONE, MAIL, OBSERVATION, ACTIVE) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, '1')")
 	if err != nil {
 		panic(err.Error())
 	}
 	insertPatient.Exec(RUN, DV, names, fatherName, motherName, phone, email, observation)
 
-	fmt.Println("Paciente Ingresado con exito.")
+	fmt.Println("Paciente ingresado con exito.")
+}
+
+func insertUser(connectionEstablished *sql.DB) {
+
+	var RUN int
+	fmt.Print("Ingresa tu RUN: ")
+	fmt.Scanln(&RUN)
+
+	var DV string
+	fmt.Print("Ingresa tu digito verificador: ")
+	fmt.Scanln(&DV)
+
+	fmt.Print("Ingresa tu(s) nombre(s): ")
+	n := bufio.NewReader(os.Stdin)
+	names, _ := n.ReadString('\n')
+
+	var fatherName string
+	fmt.Print("Ingresa tu apellido paterno: ")
+	fmt.Scanln(&fatherName)
+
+	var motherName string
+	fmt.Print("Ingresa tu apellido materno: ")
+	fmt.Scanln(&motherName)
+
+	var email string
+	fmt.Print("Ingresa el correo electronico del paciente: ")
+	fmt.Scanln(&email)
+
+	var password string
+	fmt.Print("Ingresa tu contrasena: ")
+	fmt.Scanln(&password)
+
+	insertUser, err := connectionEstablished.Prepare("INSERT INTO users (ID, RUN, DV, NAMES, FATHERNAME, MOTHERNAME, EMAIL, PASSWORD, ACTIVE) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, '1');")
+	if err != nil {
+		panic(err.Error())
+	}
+	insertUser.Exec(RUN, DV, names, fatherName, motherName, email, password)
+
+	fmt.Println("Usuario ingresado con exito.")
+
 }
